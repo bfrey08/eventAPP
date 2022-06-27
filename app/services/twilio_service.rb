508fbc8036700fc329510b0baadaf571a474e188
bfrey08 <bfrey16@gmail.com>
2022-06-27 17:04:34 -0600

    add base_url credential for production and development

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -8,7 +8,7 @@ class TwilioService
     from = Rails.application.credentials.twilio_from_number
     client = Twilio::REST::Client.new(account_sid, auth_token)
     attendees.each do |attendee|
-      body = "you are invited to '#{attendee.event.name}' by #{attendee.event.attendees.first.name}, join us: http://localhost:3000/events/#{attendee.event.id}/attendees/#{attendee.id}/login/#{attendee.api_key}"
+      body = "you are invited to '#{attendee.event.name}' by #{attendee.event.attendees.first.name}, join us: #{Rails.application.credentials.base_url}/events/#{attendee.event.id}/attendees/#{attendee.id}/login/#{attendee.api_key}"
       client.messages.create(from: from, to: attendee.phone_number, body: body)
     end
   end
