0e7bde7a055c4f4d176cbeb1222bf323a4c70298
bfrey08 <bfrey16@gmail.com>
2022-07-06 18:42:41 -0600

    heroku redis update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
